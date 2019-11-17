edfcdf7f918b02f81f91fdf9d9b0d6b2fd3f8187
Grzegorz Dąbrowski <amakuzaxx@gmail.com>
2019-11-17 14:30:09 +0100

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,1 +1,7 @@
-init
+RegisterCommand("devprint", function(source, args, rawCommand)
+    print("DEV PRINT")
+end, false)
+
+AddEventHandler("playerSpawned", function() -- Reveal whole map on spawn
+    Citizen.InvokeNative(0x4B8F743A4A6D2FF8, true)
+end)
