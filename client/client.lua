c5c725049094347e48c0175989c66a33e1d38114
Grzegorz Dąbrowski <amakuzaxx@gmail.com>
2019-11-17 15:33:00 +0100

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -2,6 +2,33 @@ RegisterCommand("devprint", function(source, args, rawCommand)
     print("DEV PRINT")
 end, false)
 
-AddEventHandler("playerSpawned", function() -- Reveal whole map on spawn
+RegisterCommand("respawn", function(source, args, rawCommand)
+    local ped = Citizen.InvokeNative(0x275F255ED201B937, source)
+    print(ped)
+        Citizen.InvokeNative(0x71BC8E838B9C6035, ped)
+end, false)
+
+AddEventHandler("onClientResourceStart", function() -- Reveal whole map on spawn
     Citizen.InvokeNative(0x4B8F743A4A6D2FF8, true)
 end)
+
+
+Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait(100)
+    if Citizen.InvokeNative(0x2E9C3FCB6798F397) then -- if is dead
+        print ("PLAYER IS DEAD")
+        Citizen.Wait(15000)
+        respawn()
+    else
+        print("NOT DEAD")
+    end
+
+  end
+end)
+
+function respawn(source)
+    local ped = Citizen.InvokeNative(0x275F255ED201B937, source)
+    print(ped)
+    Citizen.InvokeNative(0x71BC8E838B9C6035, ped)
+end
