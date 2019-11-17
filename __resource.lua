020bf2075dfe5d8d16e5b22dc37535cccaca2e39
Grzegorz Dąbrowski <amakuzaxx@gmail.com>
2019-11-17 18:13:14 +0100

    1

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,9 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 client_scripts {
-    'client/client.lua'
+    'client/client.lua',
+	'config.lua',
+	'spawnpoints.lua',
 }
 
 server_scripts {
---
+++ config.lua
@@ -0,0 +1,16 @@
+Config	= {}
+
+
+
+
+
+
+
+Config.RespawnTime = 30000
+Config.LocaleDead = "Nie żyjesz"
+Config.LocaleTimer = "Odrodzenie dostępne za "
+Config.RespawnCommand = true
+Config.kysCommand = true
+Config.Coords = true
+Config.Timecycle = "CrossLine01"
+Config.TimecycleStrenght = 1.0
