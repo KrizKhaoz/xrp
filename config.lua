37c023099406ccd7ef03426363fff89f81f22bb0
Grzegorz Dąbrowski <amakuzaxx@gmail.com>
2019-11-17 17:47:12 +0100

    Added Config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config	= {}
+
+
+
+
+
+
+
+Config.RespawnTime = 30000
+Config.LocaleDead = "Nie żyjesz"
+Config.LocaleTimer = "Odrodzenie dostępne za "
+Config.RespawnCommand = true
+Config.kysCommand = true
